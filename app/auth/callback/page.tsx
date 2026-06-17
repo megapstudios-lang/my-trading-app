81c6f588b5499f7083ee996ed463d484fcd715c0
megapstudios-lang <megapstudios@gmail.com>
2026-06-18 01:53:32 +0300

    Add callback page for authentication process

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+export default function CallbackPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const code = searchParams.get('code');
+
+    if (code) {
+      router.push('/');
+    }
+  }, [router, searchParams]);
+
+  return <p>Logging you in...</p>;
+}
